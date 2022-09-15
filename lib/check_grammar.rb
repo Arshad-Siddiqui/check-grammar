a43be84a09ba9518aa9a5ddeed6ccc5711d750c5
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-15 11:02:14 +0100

    Complete test for check_grammar

--- lib/check_grammar.rb
+++ lib/check_grammar.rb
@@ -1,3 +1,5 @@
 def check_grammar(sentence)
-  true
+  sentence.match? /^[A-Z][\w\s]*[?.!]$/
 end
+
+puts check_grammar('Yo this is some good chilli')
--- spec/check_grammar_spec.rb
+++ spec/check_grammar_spec.rb
@@ -3,15 +3,40 @@ require 'check_grammar'
 RSpec.describe '#check_grammar' do
   context 'String is uncapitalised' do
     it 'fails with "."' do
-      check_grammar('some meh grammar.')
+      result = check_grammar('some meh grammar.')
+      expect(result).to eq false
     end
 
     it "fails with '?'" do
-      check_grammar('i don\'t think this grammar is acceptable?')
+      result = check_grammar('i don\'t think this grammar is acceptable?')
+      expect(result).to eq false
     end
 
     it "fails with '!'" do
-      check_grammar('sadly this isn\'t!')
+      result = check_grammar('sadly this isn\'t!')
+      expect(result).to eq false
+    end
+
+    it 'fails without ending punctuation' do
+      result = check_grammar('some horrendous grammer')
+      expect(result).to eq false
+    end
+  end
+
+  context 'String is capitalised' do
+    it 'outputs true while ending with "."' do
+      result = check_grammar('Some mighty fine grammar.')
+      expect(result).to eq true
+    end
+
+    it 'outputs true while ending with "?"' do
+      result = check_grammar('This grammar is still good?')
+      expect(result).to eq true
+    end
+
+    it 'outputs true while ending with "!"' do
+      result = check_grammar('Clearly this is still fine!')
+      expect(result).to eq true
     end
   end
 end
